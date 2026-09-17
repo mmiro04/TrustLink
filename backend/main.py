d69d78c1ae130def4268158b03b0c60977122c6c
mmiro04 <mari.mirinashvili@gmail.com>
2026-09-17 15:20:42 +0400

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from backend.scanner.url_analyzer import analyze_url
 from backend.scanner.scoring import calculate_risk
 from backend.scanner.dns_analyzer import analyze_dns
 from backend.scanner.ssl_analyzer import analyze_ssl
+from backend.scanner.redirect_analyzer import analyze_redirects
 
 app = FastAPI(title="LinkTrust API")
 
@@ -25,9 +26,12 @@ def scan_url(request: URLRequest):
 
 	dns = None
 	ssl_info = None
+	redirects = None
 
 	if analysis["valid"] and not analysis["is_ip"]:
 		dns = analyze_dns(analysis["domain"])
+
+		redirects = analyze_redirects(request.url)
 
 		if analysis["https"]:
 			ssl_info = analyze_ssl(
@@ -41,7 +45,9 @@ def scan_url(request: URLRequest):
 		"analysis": analysis,
 		"dns": dns,
 		"ssl": ssl_info,
+		"redirects": redirects,
 		"risk": risk
+
 	}
 
 
