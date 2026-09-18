dca016a44261aca6eeb017a62d78f22b4374ae04
mmiro04 <mari.mirinashvili@gmail.com>
2026-09-18 22:24:41 +0400

    Connect frontend to deployed API

--- frontend/app.js
+++ frontend/app.js
@@ -34,7 +34,7 @@ async function scanURL() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/scan",
+            "https://linktrust-api.onrender.com/scan",
             {
                 method: "POST",
 
