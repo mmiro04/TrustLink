9574b4ca257811f804bdba7cfcb9ef1702d4bdff
mmiro04 <mari.mirinashvili@gmail.com>
2026-09-18 22:33:44 +0400

    allow github pages frontend origin

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
 	CORSMiddleware,
 	allow_origins=[
 		"http://localhost:5500",
-		"http://127.0.0.1:5500"
+		"http://127.0.0.1:5500",
+		"https://mmiro04.github.io"
 	],
 	allow_credentials=True,
 	allow_methods=["*"],
